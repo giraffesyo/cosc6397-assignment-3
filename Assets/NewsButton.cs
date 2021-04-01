328b66eaa7b01f5540861fab4cccd74928a4d9d0
Michael McQuade <michael@giraffesyo.io>
2021-04-01 14:14:18 -0500

    Randomly place windows

--- Assets/NewsButton.cs
+++ Assets/NewsButton.cs
@@ -8,11 +8,17 @@ public class NewsButton : MonoBehaviour
     // Start is called before the first frame update
 
     private NewsItem news;
+    private bool open = false;
     //TODO: Generate this window later so we can have multiple news windows open
     private NewsWindow newsWindow;
+    [SerializeField]
+    private Transform upperLeftBounds;
+    [SerializeField]
+    private Transform lowerRightBounds;
+    public GameObject NewsWindowPrefab;
     void Start()
     {
-        newsWindow = FindObjectOfType<NewsWindow>();
+
     }
 
     public void SetNewsItem(NewsItem item)
@@ -30,8 +36,20 @@ public class NewsButton : MonoBehaviour
 
     public void NewsButtonTouched()
     {
-        newsWindow.SetTitle(news.title);
-        newsWindow.SetSummary(news.summary);
-        newsWindow.SetImage(news.image);
+        if (!open)
+        {
+            float xPos = Random.Range(upperLeftBounds.position.x, lowerRightBounds.position.x);
+            float yPos = Random.Range(upperLeftBounds.position.y, lowerRightBounds.position.y);
+            Vector3 newsWindowPostion = new Vector3(xPos, yPos, 0);
+            GameObject sceneContent = GameObject.Find("SceneContent");
+            GameObject newsWindowObj = Instantiate(NewsWindowPrefab, sceneContent.transform);
+            newsWindowObj.transform.position = newsWindowPostion;
+            newsWindow = newsWindowObj.GetComponent<NewsWindow>();
+            newsWindow.SetTitle(news.title);
+            newsWindow.SetSummary(news.summary);
+            newsWindow.SetImage(news.image);
+        }
+        open = true;
+
     }
 }
